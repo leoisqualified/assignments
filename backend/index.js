c9fb4451e51934cd1e3facf393da48ab22f12d3c
leoisqualified <eaadenutsi@st.ug.edu.gh>
2025-01-19 21:38:05 +0000

    implemented server

---
+++ backend/index.js
@@ -0,0 +1,9 @@
+import express from "express";
+
+const app = express();
+const port = 3000;
+// Middleware to parse JSON request bodies
+
+app.listen(port, () => {
+  console.log(`listening on port ${port}`);
+});
